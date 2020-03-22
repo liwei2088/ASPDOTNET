5494569528970be8633c516094ae6f434177675a
liwei2088 <liwei2088@sohu.com>
2020-03-22 10:12:13 +0800

    Signed-off-by: liwei2088 <liwei2088@sohu.com>

---
+++ AspNetBasic/Basic0203/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic0203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = a + 123;
+            Console.WriteLine("result:" + b);
+        }
+    }
+}
